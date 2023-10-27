0be07e8bc066f63daeb980458ec3365c42b2c8cf
Uberlan Torres <uberlan9@gmail.com>
2023-10-27 18:51:36 -0300

    Classe Posição

--- Console_xadrez/Console_xadrez/Program.cs
+++ Console_xadrez/Console_xadrez/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Console_xadrez
+﻿using tabuleiro;
+
+namespace Console_xadrez
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+            Console.ReadLine();
 
         }
     }
