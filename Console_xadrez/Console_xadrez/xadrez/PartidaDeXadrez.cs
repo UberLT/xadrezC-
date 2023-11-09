d7326fd7d20b1292986f6a73986b92025c6e1160
Uberlan Torres <uberlan9@gmail.com>
2023-11-09 17:01:25 -0300

    Joogada especial promoção

--- Console_xadrez/Console_xadrez/xadrez/PartidaDeXadrez.cs
+++ Console_xadrez/Console_xadrez/xadrez/PartidaDeXadrez.cs
@@ -135,6 +135,19 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque");
             }
 
+            Peca p = tab.peca(destino);
+
+            //Jogada especial promoção
+            if(p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7));
+                p = tab.retirarPeca(destino);
+                pecas.Remove(p);
+                Peca dama = new Dama(tab, p.cor);
+                tab.colocarPeca(dama, destino);
+                pecas.Add(dama);
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
